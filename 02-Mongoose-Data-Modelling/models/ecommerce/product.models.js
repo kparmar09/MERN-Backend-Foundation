c51904e0d3b89b22448f36ef9a98b51f50265914
Krishna Parmar <krishnaparmar850@gmail.com>
2024-06-14 00:06:39 +0530

    Second Commit

---
+++ 02-Mongoose-Data-Modelling/models/ecommerce/product.models.js
@@ -0,0 +1,37 @@
+import mongoose from "mongoose";
+
+const productSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+    },
+    description: {
+      type: String,
+      required: true,
+    },
+    productImage: {
+      type: String,
+    },
+    price: {
+      type: Number,
+      default: 0,
+    },
+    stock: {
+      type: Number,
+      default: 0,
+    },
+    owner: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+    },
+    category: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Category",
+      required: true,
+    },
+  },
+  { timestamps }
+);
+
+export const Product = mongoose.model("Product", productSchema);
