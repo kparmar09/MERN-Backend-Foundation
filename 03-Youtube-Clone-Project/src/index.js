1a4f03e0070f40beaf3b16f75b8a49eb94c61c31
Krishna Parmar <krishnaparmar850@gmail.com>
2024-06-14 20:59:15 +0530

    MongoDB Setup

--- 03-Youtube-Clone-Project/src/index.js
+++ 03-Youtube-Clone-Project/src/index.js
@@ -1,8 +1,15 @@
+import dotenv from "dotenv";
 import express from "express";
+import connectDB from "./db/index.js";
 
-const app = express();
-const port = 3000;
-
-app.listen(port, () => {
-  console.log(`Server running on port ${port}`);
+dotenv.config({
+  path: "../env",
 });
+
+connectDB();
+
+// const app = express();
+
+// app.listen(process.env.PORT, () => {
+//   console.log(`Server running on port ${process.env.PORT}`);
+// });
