1f267b8db1066f44c09d8f970bdcbef97c58fab6
Krishna Parmar <krishnaparmar850@gmail.com>
2024-06-16 18:30:38 +0530

    Add Register Controller

--- 03-Youtube-Clone-Project/src/controllers/user.controller.js
+++ 03-Youtube-Clone-Project/src/controllers/user.controller.js
@@ -1,9 +1,70 @@
 import { asyncHandler } from "../utils/asyncHandler.js";
+import { ApiError } from "../utils/ApiError.js";
+import { ApiResponse } from "../utils/ApiResponse.js";
+import { User } from "../models/user.model.js";
+import { uploadOnCloudinary } from "../utils/cloudinary.js";
 
-const registerUser = asyncHandler((req, res) => {
-  res.status(200).json({
-    message: "ok",
+const registerUser = asyncHandler(async (req, res) => {
+  // 1. Data from frontend
+  const { username, fullname, email, password } = req.body;
+  console.log("email: ", email);
+
+  // 2. Validation
+  if (
+    [fullname, username, email, password].some((field) => field?.trim() === "")
+  ) {
+    throw new ApiError(400, "All fields are required");
+  }
+  if (!email.includes("@")) {
+    throw new ApiError(400, "Email field is invalid");
+  }
+
+  const existingEmail = User.findOne({ email: email });
+  if (existingEmail) {
+    throw new ApiError(409, "User with email already exists");
+  }
+
+  const existingUsername = User.findOne({ username: username });
+  if (existingUsername) {
+    throw new ApiError(409, "User with this username already exists");
+  }
+
+  // 3. Upload image to cloudinary and check avatar as it's a required field
+  console.log(req.files);
+  const avatarLocalPath = req.files?.avatar[0]?.path;
+  const coverImageLocalPath = req.files?.coverImage[0]?.path;
+  if (!avatarLocalPath) {
+    throw new ApiError(400, "Avatar file is required");
+  }
+
+  const avatar = await uploadOnCloudinary(avatarLocalPath); // await is required as uploading is happening
+  const coverImage = await uploadOnCloudinary(coverImageLocalPath);
+  if (!avatar) {
+    throw new ApiError(400, "Avatar file is required");
+  }
+
+  // 4. Create User object and create entry in db
+  const user = await User.create({
+    fullname,
+    avatar: avatar.url,
+    coverImage: coverImage?.url || "",
+    email,
+    password,
+    username: username.toLowerCase(),
   });
+
+  // 5. Check if user is created or not and remove the password and createdUser field from response
+  const createdUser = await User.findById(user._id).select(
+    "-password -refreshToken"
+  );
+  if (!createdUser) {
+    throw new ApiError(500, "Something went wrong while registering the user.");
+  }
+
+  // 6. Return response
+  return res
+    .status(201)
+    .json(new ApiResponse(200, createdUser, "User registered successfully"));
 });
 
 export { registerUser };
--- 03-Youtube-Clone-Project/src/models/user.model.js
+++ 03-Youtube-Clone-Project/src/models/user.model.js
@@ -50,7 +50,7 @@ const userSchema = new mongoose.Schema(
 // Method to save encrypted password in database just before saving through 'pre' middleware
 userSchema.pre("save", async function (next) {
   if (!this.isModified("password")) return next();
-  this.password = bcrypt.hash(this.password, 10);
+  this.password = await bcrypt.hash(this.password, 10);
   next();
 });
 
