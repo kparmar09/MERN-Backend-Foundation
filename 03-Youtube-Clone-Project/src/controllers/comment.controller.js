bc99364468aa8dc983d4c89a61110f1197c3a2fe
Krishna Parmar <krishnaparmar850@gmail.com>
2024-06-20 18:30:36 +0530

    Add commentAggregate to getVideoComments

--- 03-Youtube-Clone-Project/src/controllers/comment.controller.js
+++ 03-Youtube-Clone-Project/src/controllers/comment.controller.js
@@ -3,11 +3,58 @@ import { ApiResponse } from "../utils/ApiResponse.js";
 import { asyncHandler } from "../utils/asyncHandler.js";
 import { Comment } from "../models/comment.model.js";
 import { Video } from "../models/video.model.js";
+import mongoose from "mongoose";
 
 const getVideoComments = asyncHandler(async (req, res) => {
-  //TODO: get all comments for a video
   const { videoId } = req.params;
   const { page = 1, limit = 10 } = req.query;
+
+  const commentAggregate = await Comment.aggregate([
+    { $match: { video: new mongoose.Types.ObjectId(videoId) } },
+    {
+      $lookup: {
+        from: "users",
+        localField: "owner",
+        foreignField: "_id",
+        as: "ownerInfo",
+        pipeline: [
+          {
+            $project: {
+              _id: 1,
+              username: 1,
+              fullname: 1,
+              email: 1,
+            },
+          },
+        ],
+      },
+    },
+    {
+      $unwind: {
+        path: "$ownerInfo",
+        preserveNullAndEmptyArrays: true,
+      },
+    },
+  ]);
+
+  console.log(commentAggregate);
+
+  if (!commentAggregate.length) {
+    throw new ApiError(
+      400,
+      "Invalid video id or something went wrong while fetching comments"
+    );
+  }
+
+  return res
+    .status(200)
+    .json(
+      new ApiResponse(
+        200,
+        { commentAggregate },
+        "Comments to the requested video fetched"
+      )
+    );
 });
 
 const addComment = asyncHandler(async (req, res) => {
